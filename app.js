dfadbb86bc127722314612ff5b5774af0d3a06f4
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-15 12:43:41 +0300

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
+const cors = require('cors');
 const { rateLimit } = require('express-rate-limit');
 const NotFoundError = require('./errors/not-found-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -11,6 +12,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/movies' } = process.env;
 
 const app = express();
+
+app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,

6d83e76bab5abd0d55c36904911ef688dea3a533
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-17 16:16:58 +0300

    refactor

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const errorServer = require('./middlewares/errorServer');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/movies' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
 app.use(cors());
-
-app.use(limiter);
 
 app.use(helmet());
 app.use(bodyParser.json());
@@ -28,6 +26,8 @@ mongoose.connect(DB_URL, {
 });
 
 app.use(requestLogger);
+
+app.use(limiter);
 
 app.use('/', router);
 

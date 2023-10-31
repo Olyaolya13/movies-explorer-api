2853cbcd70a143c015185c36749f453e1956ab1b
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-10-31 13:43:40 +0300

    refactor

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,12 @@
-const { HTTP_STATUS_CREATED, HTTP_STATUS_OK } = require('http2').constants;
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/user');
+const { HTTP_STATUS_CREATED, HTTP_STATUS_OK } = require("http2").constants;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
-const { JWT_SECRET = 'secret-key' } = process.env;
+const { JWT_SECRET = "secret-key" } = process.env;
 
-const BadRequestError = require('../errors/bad-request-error');
-const ConflictError = require('../errors/conflict-error');
+const BadRequestError = require("../errors/bad-request-error");
+const ConflictError = require("../errors/conflict-error");
 
 module.exports.getUserInfo = (req, res, next) => {
   User.findById(req.user._id)
@@ -20,20 +20,24 @@ module.exports.updateUserInfo = (req, res, next) => {
   const UserId = req.user._id;
   const { name, email } = req.body;
 
-  User.findByIdAndUpdate(UserId, { name, email }, {
-    new: true,
-    runValidators: true,
-    upsert: true,
-  })
+  User.findByIdAndUpdate(
+    UserId,
+    { name, email },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    }
+  )
     .orFail()
     .then((users) => {
       res.status(HTTP_STATUS_OK).send(users);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         next(new BadRequestError(err.message));
       } else if (err.code === 11000) {
-        next(new ConflictError('Пользователь с таким email уже существует'));
+        next(new ConflictError("Пользователь с таким email уже существует"));
       } else {
         next(err);
       }
@@ -46,7 +50,7 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-        expiresIn: '1w',
+        expiresIn: "1w",
       });
       res.send({ token });
     })
@@ -56,16 +60,17 @@ module.exports.login = (req, res, next) => {
 };
 
 module.exports.createUsers = (req, res, next) => {
-  const {
-    name, email, password,
-  } = req.body;
+  const { name, email, password } = req.body;
   // хешируем пароль
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create(
-      {
-        name, email, password: hash,
-      },
-    ))
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        email,
+        password: hash,
+      })
+    )
     .then((user) => {
       res.status(HTTP_STATUS_CREATED).send({
         name: user.name,
@@ -75,8 +80,8 @@ module.exports.createUsers = (req, res, next) => {
     })
     .catch((err) => {
       if (err.code === 11000) {
-        next(new ConflictError('Пользователь с данным email уже существует'));
-      } else if (err.name === 'ValidationError') {
+        next(new ConflictError("Пользователь с данным email уже существует"));
+      } else if (err.name === "ValidationError") {
         next(new BadRequestError(err.message));
       } else {
         next(err);
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken');
-const UnAuthorizedError = require('../errors/unauthorized-error');
+const jwt = require("jsonwebtoken");
+const UnAuthorizedError = require("../errors/unauthorized-error");
 
-const { JWT_SECRET = 'secret-key' } = process.env;
+const { JWT_SECRET = "secret-key" } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnAuthorizedError('Необходима авторизация'));
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    next(new UnAuthorizedError("Необходима авторизация"));
     return;
   }
 
-  const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace("Bearer ", "");
   let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new UnAuthorizedError('Необходима авторизация'));
+    next(new UnAuthorizedError("Необходима авторизация"));
     return;
   }
 

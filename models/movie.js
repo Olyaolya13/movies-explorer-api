14466aec86d971c47b2ec1d663085b6a5c8f614a
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-11-01 17:50:54 +0300

    Revert "refactor"
    This reverts commit 2853cbcd70a143c015185c36749f453e1956ab1b.

--- models/movie.js
+++ models/movie.js
@@ -1,82 +1,81 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-const movieSchema = new mongoose.Schema({
-  country: {
-    type: String,
-    required: true,
-    minlength: [2, 'Минимальная длина поля "country" - 2'],
-    maxlength: [30, 'Максимальная длина поля "country" - 30'],
-  },
-  director: {
-    type: String,
-    required: true,
-    minlength: [2, 'Минимальная длина поля "director" - 2'],
-  },
-  duration: {
-    type: Number,
-    required: true,
-    minlength: [1, 'Минимальная длина поля "duration" - 1'],
-
-  },
-  year: {
-    type: String,
-    required: true,
-    minlength: [4, 'Минимальная длина поля "year" - 4'],
-    maxlength: [4, 'Максимальная длина поля "year" - 4'],
-  },
-  description: {
-    type: String,
-    required: true,
-    minlength: [2, 'Минимальная длина поля "description" - 2'],
-    maxlength: [250, 'Максимальная длина поля "description" - 250'],
-  },
-  image: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректный Url',
+const movieSchema = new mongoose.Schema(
+  {
+    country: {
+      type: String,
+      required: true,
+      minlength: [2, 'Минимальная длина поля "country" - 2'],
+      maxlength: [30, 'Максимальная длина поля "country" - 30'],
+    },
+    director: {
+      type: String,
+      required: true,
+      minlength: [2, 'Минимальная длина поля "director" - 2'],
+    },
+    duration: {
+      type: Number,
+      required: true,
+      minlength: [1, 'Минимальная длина поля "duration" - 1'],
+    },
+    year: {
+      type: String,
+      required: true,
+      minlength: [4, 'Минимальная длина поля "year" - 4'],
+      maxlength: [4, 'Максимальная длина поля "year" - 4'],
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: [2, 'Минимальная длина поля "description" - 2'],
+    },
+    image: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: "Некорректный Url",
+      },
+    },
+    trailerLink: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: "Некорректный Url",
+      },
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: "Некорректный Url",
+      },
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    movieId: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    nameRU: {
+      type: String,
+      required: true,
+      minlength: [2, 'Минимальная длина поля "nameRU" - 2'],
+    },
+    nameEN: {
+      type: String,
+      required: true,
+      minlength: [2, 'Минимальная длина поля "nameEN" - 2'],
     },
   },
-  trailerLink: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректный Url',
-    },
-  },
-  thumbnail: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректный Url',
-    },
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  movieId: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  nameRU: {
-    type: String,
-    required: true,
-    minlength: [2, 'Минимальная длина поля "nameRU" - 2'],
-    maxlength: [30, 'Максимальная длина поля "nameRU" - 30'],
-  },
-  nameEN: {
-    type: String,
-    required: true,
-    minlength: [2, 'Минимальная длина поля "nameEN" - 2'],
-    maxlength: [30, 'Максимальная длина поля "nameEN" - 30'],
-  },
-}, { versionKey: false });
+  { versionKey: false }
+);
 
-module.exports = mongoose.model('movie', movieSchema);
+module.exports = mongoose.model("movie", movieSchema);
